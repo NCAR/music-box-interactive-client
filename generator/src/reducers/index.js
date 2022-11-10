45e895ef011ab704ddb9cd6f2888234d8e518715
Shane Ng <shanengys@gmail.com>
2022-11-10 16:12:02 -0600

    add TODOs

--- generator/src/reducers/index.js
+++ generator/src/reducers/index.js
@@ -6,9 +6,19 @@ const reactionTypeReducer = (reactionType = utils.reaction_types.GAS, action) =>
         console.log(action.payload)
         return action.payload;
     }
-
     return reactionType;
 }
+
+const speciesConfigReducer = (speciesConfigs, action) => {
+    if (action.type === utils.action_types.ADD_NEW_SPECIES) {
+        speciesConfigs = [...speciesConfigs, action.payload]
+    } else if (action.type === utils.action_types.CHANGE_EXISTING_SPECIES) {
+        // TODO
+    }
+
+    return speciesConfigs
+}
+
 
 export default combineReducers({
     reactionType: reactionTypeReducer
