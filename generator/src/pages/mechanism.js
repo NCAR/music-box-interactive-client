b1f398207ed301954deebc2a8850db5f82e3c8c1
Matt Dawson <mattdawson@ucar.edu>
2023-04-14 17:12:42 -0700

    temporarily remove aerosol options in mechanism

--- generator/src/pages/mechanism.js
+++ generator/src/pages/mechanism.js
@@ -3,53 +3,31 @@ import { connect } from 'react-redux';
 import { Link } from "gatsby";
 import Layout from "../components/Layout";
 import GasSpeciesList from "../components/GasSpeciesList.js";
-import utils from"../utils";
 import { changeReactionType } from '../actions'
 
 function Mechanism(props) {
   const SPECIES = 0
   const REACTIONS = 1
-  const type = props.state.reactionType;
   const [param, setParam] = useState(SPECIES)
 
-  const typeSelected = "btn btn-primary btn-lg rounded-0"
-  const typeNotSelected = "btn btn-secondary btn-lg rounded-0"
-  const paramSelected = "btn btn-primary rounded-0"
-  const paramNotSelected = "btn btn-secondary rounded-0"
-
-  const aerosolClickHandler = () => {
-    props.changeReactionType(utils.reaction_types.AEROSOL)
-    setParam(SPECIES)
-  }
-
-  const gasClickhandler = () => {
-    props.changeReactionType(utils.reaction_types.GAS)
-  }
+  const paramSelected = "btn btn-primary btn-ncar-active"
+  const paramNotSelected = "btn btn-secondary"
 
   return (
     <Layout>
         <main role="main">
-          <br />
-          <div className="container text-left mx-5">
-            <div className="pt-3">
-              <button className={type === utils.reaction_types.GAS ?  typeSelected : typeNotSelected} onClick={ gasClickhandler }>Gas</button>
-              <button className={type === utils.reaction_types.AEROSOL ? typeSelected : typeNotSelected} onClick={ aerosolClickHandler }>Aerosol</button>
-            </div>
-          </div>
-          
           <div className="container text-center">
             <div className="pt-3">
               <button className={param === SPECIES ?  paramSelected : paramNotSelected} onClick={() => setParam(SPECIES)}>
-                Chemical Species
+                Gas Species
               </button>
-              {type === utils.reaction_types.GAS ? <button className={param === REACTIONS ? paramSelected : paramNotSelected} onClick={() => setParam(REACTIONS)}>Reactions</button> : null}
+              <button className={param === REACTIONS ? paramSelected : paramNotSelected} onClick={() => setParam(REACTIONS)}>Reactions</button>
             </div>
           </div>
 
           {
             param === SPECIES ? <p className="lead-muted p-2">Select a chemical species from the list to view/edit its properties, or add a new chemical species to the mechansim. The chemical species you add here will be available to participate in <Link to="mechanism/reactions">reactions</Link> and can be include in the <Link to="conditions">model conditions</Link>.</p> : null
           }
-          
           {param === SPECIES ? <GasSpeciesList /> : <ReactionsList />}
 
         </main>
