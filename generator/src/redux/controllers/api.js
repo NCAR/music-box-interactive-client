d1442d7b5dc7903482452878644013bdd473b7f7
Kyle Shores <kyle.shores44@gmail.com>
2023-04-19 09:40:46 -0500

    loading api URL from environment file

--- generator/src/redux/controllers/api.js
+++ generator/src/redux/controllers/api.js
@@ -1,13 +1,11 @@
 const axios = require('axios');
 
-const API_BASE_URL = 'http://localhost:8000';
-
 async function fetchExample(example) {
   try {
     const params = {
       example: example
     };
-    const response = await axios.get(`${API_BASE_URL}/api/load-example`, { params: params });
+    const response = await axios.get(`${process.env.GATSBY_API_URL}/api/load-example`, { params: params });
     return response.data;
   } catch (error) {
     console.error(`Error fetching example ${example}: ${error.message}`);
