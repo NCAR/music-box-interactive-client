aca728d8758fdb5dd1e6fbabe48d5aab154fbbc1
Kyle Shores <kyle.shores44@gmail.com>
2023-04-20 16:05:34 -0500

    replacing old nav stuff with react-bootstrap

--- generator/src/components/GasSpeciesTab.js
+++ generator/src/components/GasSpeciesTab.js
@@ -11,7 +11,7 @@ function GasSpeciesTab(props) {
   return (
     <>
       <p className="lead-muted p-2">Select a chemical species from the list to view/edit its properties, or add a new chemical species to the mechansim. The chemical species you add here will be available to participate in <Link to="mechanism/reactions">reactions</Link> and can be include in the <Link to="conditions">model conditions</Link>.</p>
-      <div id="species-content" className="container-fluid p-2 d-flex flex-column vh-100 overflow-hidden">
+      <div id="species-content" className="container-fluid p-2 d-flex flex-column overflow-hidden">
         <div className="row flex-grow-1 overflow-hidden">
           <div className="col-md-4 col-lg-4 mh-100 overflow-auto">
             <div className="row flex-shrink-0">
--- generator/src/components/NavBar.js
+++ generator/src/components/NavBar.js
@@ -4,52 +4,57 @@ import { Link } from "gatsby"
 import { useDispatch, connect } from 'react-redux';
 import { doRun } from '../redux/actions';
 import { getMechanismAsObject } from "../redux/selectors";
+import { navigate } from 'gatsby';
+import { Nav } from "react-bootstrap";
 
 function NavBar(props) {
-    const activeColor = {color: "#000080"}
-    const dispatch = useDispatch();
+  const activeColor = { color: "#000080" }
+  const dispatch = useDispatch();
 
-    const handleClick = () => {
-      dispatch(doRun(props.mechanism));
-    }
+  const handleClick = () => {
+    dispatch(doRun(props.mechanism));
+    navigate("/results");
+  }
 
-    return (
-        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-ncar-menu sidebar collapse">
-          <div className="position-sticky pt-3">
-            <div id="main-nav" className="nav flex-column pt-2">
-              <small className="nav-section">SETUP</small>
-              <Link className="nav-link" to="/getting_started" activeStyle={activeColor}>
-                <span className="oi oi-signpost oi-prefix"></span>
-                  Start Here
-              </Link>
-              <Link className="nav-link" to="/mechanism" activeStyle={activeColor}>
-                <span className="oi oi-random oi-prefix"></span>
-                Mechanism
-              </Link>
-              <Link className="nav-link" to="/conditions" activeStyle={activeColor}>
-                <span className="oi oi-dashboard oi-prefix"></span>
-                Conditions
-              </Link>
-              <div className="pt-1 pb-3 mx-0 my-2" style={{borderTop: '1px solid gray', borderBottom: '1px solid gray'}}>
-                <small className="nav-section">RUN</small>
-                <div style={{textAlign:'center'}}>
-                  <button id="run-model" style={{margin: 'auto'}} className="btn btn-primary btn-ncar-active" onClick={handleClick}>Run Model</button>
-                </div>
-              </div>
-              <div id="post-run-links">
-              </div>
-            </div>
-          </div>
-          <div className="mt-auto mb-4" style={{position: 'absolute', bottom: 0, width: '100%'}}>
-            <img id="logo" className="img-fluid p-2" alt="" src={logo} />
-          </div>
-        </nav>
-    )
+  return (
+    <Nav>
+      <small className="nav-section">SETUP</small>
+      <Link className="nav-link" to="/getting_started" activeStyle={activeColor}>
+        <span className="oi oi-signpost oi-prefix"></span>
+        Start Here
+      </Link>
+      <Link className="nav-link" to="/mechanism" activeStyle={activeColor}>
+        <span className="oi oi-random oi-prefix"></span>
+        Mechanism
+      </Link>
+      <Link className="nav-link" to="/conditions" activeStyle={activeColor}>
+        <span className="oi oi-dashboard oi-prefix"></span>
+        Conditions
+      </Link>
+      <div className="pt-1 pb-3 mx-0 my-2" style={{ borderTop: '1px solid gray', borderBottom: '1px solid gray' }}>
+        <small className="nav-section">RUN</small>
+        <div style={{ textAlign: 'center' }}>
+          <button id="run-model" style={{ margin: 'auto' }} className="btn btn-primary btn-ncar-active" onClick={handleClick}>Run Model</button>
+        </div>
+      </div>
+    </Nav>
+    // <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-ncar-menu sidebar collapse">
+    //   <div className="position-sticky pt-3">
+    //     <div id="main-nav" className="nav flex-column pt-2">
+    //       <div id="post-run-links">
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="mt-auto mb-4" style={{position: 'absolute', bottom: 0, width: '100%'}}>
+    //     <img id="logo" className="img-fluid p-2" alt="" src={logo} />
+    //   </div>
+    // </nav>
+  )
 }
 
 const mapStateToProps = state => {
-    const mechanism = getMechanismAsObject(state);
-    return mechanism;
+  const mechanism = getMechanismAsObject(state);
+  return mechanism;
 };
 
 export default connect(mapStateToProps)(NavBar);
