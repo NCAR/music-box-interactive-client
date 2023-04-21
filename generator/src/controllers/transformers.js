a772f10c4ae9af7164c431f3e2fd2193efb2f1a8
Kyle Shores <kyle.shores44@gmail.com>
2023-04-21 16:20:05 -0500

    parsing all reactions in javascript

--- generator/src/controllers/transformers.js
+++ generator/src/controllers/transformers.js
@@ -5,13 +5,15 @@ function translate_from_camp_config(config) {
   let id = 0;
   const parseReactants = (reaction) => Object.entries(reaction.reactants).map(([name, props]) => ({name: name, qty: props.qty || 1}));
   const parseProducts = (reaction) => Object.entries(reaction.products).map(([name, props]) => ({name: name, yield: props.yield || 1}));
+  const parseAlkoxyProducts = (products) => Object.entries(products).map(([name, props]) => ({name: name, yield: props.yield || 1}));
+  const parseNitrateProducts = parseAlkoxyProducts;
 
   const reactions = config['reactions'].map(reaction => {
     switch (reaction.type) {
       case ReactionTypes.ARRHENIUS: {
         return {
-          id: id++,
           ...reactionTypes.arrhenius,
+          id: id++,
           data: {
             ...reactionTypes.arrhenius.data,
             A: reaction.A || reactionTypes.arrhenius.data.A,
@@ -26,56 +28,112 @@ function translate_from_camp_config(config) {
       }
       case ReactionTypes.PHOTOLYSIS: {
         return {
-          id: id++,
           ...reactionTypes.photolysis,
+          id: id++,
           data: {
             ...reactionTypes.photolysis.data,
             reactant: parseReactants(reaction)[0].name,
-            products: parseProducts(reaction)
+            products: parseProducts(reaction),
+            scaling_factor: reaction['scaling factor'] || 1.0
           }
         }
       }
       case ReactionTypes.EMISSION: {
         return {
-          id: id++,
           ...reactionTypes.emission,
+          id: id++,
+          data: {
+            ...reactionTypes.emission.data,
+            scaling_factor: reaction['scaling factor'] || 1.0,
+            species: {name: reaction['species'], qty: 1},
+            musica_name: reaction['MUSICA name'] || ''
+          }
         }
       }
       case ReactionTypes.FIRST_ORDER_LOSS: {
         return {
-          id: id++,
           ...reactionTypes.firstOrderLoss,
+          id: id++,
+          data: {
+            ...reactionTypes.firstOrderLoss.data,
+            species: reaction['species'],
+            scaling_factor: reaction['scaling factor'] || 1.0
+          }
         }
       }
       case ReactionTypes.TERNARY_CHEMICAL_ACTIVATION: {
         return {
-          id: id++,
           ...reactionTypes.ternaryChemicalActivation,
+          id: id++,
+          data: {
+            ...reactionTypes.ternaryChemicalActivation.data,
+            k0_A: reaction['k0_A'] || 1.0,
+            k0_B: reaction['k0_B'] || 0.0,
+            k0_C: reaction['k0_C'] || 0.0,
+            kinf_A: reaction['kinf_A'] || 1.0,
+            kinf_B: reaction['kinf_B'] || 0.0,
+            kinf_C: reaction['kinf_C'] || 0.0,
+            Fc: reaction['Fc'] || 0.6,
+            N: reaction['N:'] || 1.0,
+            reactants: parseReactants(reaction),
+            products: parseProducts(reaction)
+          }
         }
       }
       case ReactionTypes.TROE: {
         return {
-          id: id++,
           ...reactionTypes.troe,
+          id: id++,
+          data: {
+            ...reactionTypes.troe.data,
+            k0_A: reaction['k0_A'] || 1.0,
+            k0_B: reaction['k0_B'] || 0.0,
+            k0_C: reaction['k0_C'] || 0.0,
+            kinf_A: reaction['kinf_A'] || 1.0,
+            kinf_B: reaction['kinf_B'] || 0.0,
+            kinf_C: reaction['kinf_C'] || 0.0,
+            Fc: reaction['Fc'] || 0.6,
+            N: reaction['N:'] || 1.0,
+            products: parseProducts(reaction),
+            reactants: parseReactants(reaction)
+          }
         }
       }
       case ReactionTypes.WENNBERG_NO_RO2: {
         return {
-          id: id++,
           ...reactionTypes.branched,
+          id: id++,
+          data: {
+              ...reactionTypes.branched.data,
+              X: reaction['X:'] || 1.0,
+              Y: reaction['Y:'] || 0.0,
+              a0: reaction['a0'] || 1.0,
+              n: reaction['n:'] || 0,
+              reactants: parseReactants(reaction),
+              primary_products: parseAlkoxyProducts(reaction['alkoxy products']),
+              secondary_products: parseNitrateProducts(reaction['nitrate products'])
+          },
         }
       }
       case ReactionTypes.WENNBERG_TUNNELING: {
         return {
-          id: id++,
           ...reactionTypes.tunneling,
+          id: id++,
+          data: {
+              ...reactionTypes.tunneling,
+              A: reaction['A:'] || 1.0,
+              B: reaction['B:'] || 0.0,
+              C: reaction['C:'] || 0.0,
+              reactants: parseReactants(reaction),
+              products: parseProducts(reaction)
+          },
         }
       }
       default:
         console.error(`Unknown reaction type: ${reaction.type}`);
+        return {id: id++, shortName() {return ""}}
     }
   })
-  console.log(reactions)
   return {
     gasSpecies: config['species'].map((species) => ({ name: species, properties: [] })),
     reactions: reactions
