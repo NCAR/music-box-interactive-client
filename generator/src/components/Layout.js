e5dab954e6a4544d997e24f239af4bca7fa0b9e1
Kyle Shores <kyle.shores44@gmail.com>
2023-04-28 10:36:07 -0500

    using a static query to grab the site title

--- generator/src/components/Layout.js
+++ generator/src/components/Layout.js
@@ -7,7 +7,7 @@ import { useDispatch, connect } from 'react-redux';
 import { Link } from "gatsby"
 import { doRun } from '../redux/actions';
 import { getMechanism, getAllConditions } from "../redux/selectors";
-import { navigate } from 'gatsby';
+import { navigate, graphql, StaticQuery } from 'gatsby';
 
 function Layout(props) {
     const activeColor = { color: "#00797C" }
@@ -18,87 +18,100 @@ function Layout(props) {
         navigate("/results");
     }
 
-    const meta = {
-        title: "MusicBox",
-        meta: {
-            charset: "UTF-8",
-            name: "viewport",
-            content: "idth=device-width, initial-scale=1"
-        }
-    }
-
     return (
-        <DocumentMeta {...meta}>
-            <div className={styles.dashboard}>
-                <Navbar className="shadow p-0 sticky-top flex-md-nowrap" bg="dark" variant="dark">
-                    <Navbar.Brand className="navbar-brand col-md-3 col-lg-2 me-0 px-3"
-                                  href="/home">MusicBox</Navbar.Brand>
-                </Navbar>
-                <div className={styles.content}>
-                    <div className={styles.menu}>
-                        <Nav className="flex-column pt-3">
-                            <small className="nav-section">SETUP</small>
-                            <Link className="nav-link" to="/getting_started" activeStyle={activeColor}>
-                                <span className="oi oi-signpost oi-prefix"></span>
-                                Start Here
-                            </Link>
-                            <Link className="nav-link" to="/mechanism" activeStyle={activeColor}>
-                                <span className="oi oi-random oi-prefix"></span>
-                                Mechanism
-                            </Link>
-                            <Link className="nav-link" to="/conditions" activeStyle={activeColor}>
-                                <span className="oi oi-dashboard oi-prefix"></span>
-                                Conditions
-                            </Link>
-                            <div className="pt-1 pb-3 mx-0 my-2" style={{ borderTop: '1px solid gray', borderBottom: '1px solid gray' }}>
-                                <small className="nav-section">RUN</small>
-                                <div style={{ textAlign: 'center' }}>
-                                    <button id="run-model" style={{ margin: 'auto' }} className="btn btn-primary btn-ncar-active" onClick={handleClick}>Run Model</button>
-                                </div>
-                            </div>
-                            <small className="nav-section">ANALYSIS</small>
-                            <Link className="nav-link" to="/plots" activeStyle={activeColor}>
-                                <span className="oi oi-graph oi-prefix"></span>
-                                 Plot Results
-                            </Link>
-                            <Link className="nav-link" to="/flow_diagram" activeStyle={activeColor}>
-                                <span className="oi oi-fork oi-prefix"></span>
-                                Flow Diagram
-                            </Link>
-                            <Link className="nav-link" to="/downloads" activeStyle={activeColor}>
-                                <span className="oi oi-data-transfer-download oi-prefix"></span>
-                                Download
-                            </Link>
-                        </Nav>
-                        <div className="mt-auto mb-4">
-                            <img id="logo" className="img-fluid p-2" alt="" src={logo} />
-                        </div>
-                    </div>
-                    <div className={styles.main}>
-                        {props.children}
-                        <div className={styles.footer}>
-                            <div className="row justify-content-around">
-                                <div className="col-md offset-md-1">
-                                    <a className="footer-link" href="/home#behind-the-music"><h5>About</h5></a>
-                                    <ul className="list-unstyled text-small">
-                                        <a className="footer-link" href="/home#collaborators"><li>Collaborators</li></a>
-                                        <a className="footer-link" href="/home#sponsors"><li>Sponsors</li></a>
-                                        <a className="footer-link" href="/home#contact"><li>Contact us</li></a>
+        <StaticQuery
+            query={graphql`
+            query HeadingQuery {
+                site {
+                    siteMetadata {
+                        title
+                        version
+                    }
+                }
+            }
+        `}
+            render={data => (
 
-                                    </ul>
+                <DocumentMeta {...{
+                    title: `${data.site.siteMetadata.title} ${data.site.siteMetadata.version}`,
+                    meta: {
+                        charset: "UTF-8",
+                        name: "viewport",
+                        content: "idth=device-width, initial-scale=1"
+                    }
+                }}>
+                    <div className={styles.dashboard}>
+                        <Navbar className="shadow p-0 sticky-top flex-md-nowrap" bg="dark" variant="dark">
+                            <Navbar.Brand className="navbar-brand col-md-3 col-lg-2 me-0 px-3"
+                                href="/home">{data.site.siteMetadata.title}</Navbar.Brand>
+                        </Navbar>
+                        <div className={styles.content}>
+                            <div className={styles.menu}>
+                                <Nav className="flex-column pt-3">
+                                    <small className="nav-section">SETUP</small>
+                                    <Link className="nav-link" to="/getting_started" activeStyle={activeColor}>
+                                        <span className="oi oi-signpost oi-prefix"></span>
+                                        Start Here
+                                    </Link>
+                                    <Link className="nav-link" to="/mechanism" activeStyle={activeColor}>
+                                        <span className="oi oi-random oi-prefix"></span>
+                                        Mechanism
+                                    </Link>
+                                    <Link className="nav-link" to="/conditions" activeStyle={activeColor}>
+                                        <span className="oi oi-dashboard oi-prefix"></span>
+                                        Conditions
+                                    </Link>
+                                    <div className="pt-1 pb-3 mx-0 my-2" style={{ borderTop: '1px solid gray', borderBottom: '1px solid gray' }}>
+                                        <small className="nav-section">RUN</small>
+                                        <div style={{ textAlign: 'center' }}>
+                                            <button id="run-model" style={{ margin: 'auto' }} className="btn btn-primary btn-ncar-active" onClick={handleClick}>Run Model</button>
+                                        </div>
+                                    </div>
+                                    <small className="nav-section">ANALYSIS</small>
+                                    <Link className="nav-link" to="/plots" activeStyle={activeColor}>
+                                        <span className="oi oi-graph oi-prefix"></span>
+                                        Plot Results
+                                    </Link>
+                                    <Link className="nav-link" to="/flow_diagram" activeStyle={activeColor}>
+                                        <span className="oi oi-fork oi-prefix"></span>
+                                        Flow Diagram
+                                    </Link>
+                                    <Link className="nav-link" to="/downloads" activeStyle={activeColor}>
+                                        <span className="oi oi-data-transfer-download oi-prefix"></span>
+                                        Download
+                                    </Link>
+                                </Nav>
+                                <div className="mt-auto mb-4">
+                                    <img id="logo" className="img-fluid p-2" alt="" src={logo} />
                                 </div>
-                                <div className="col-md">
-                                    <a className="footer-link" href="/getting_started#how-to-use"><h5>How to use</h5></a>
-                                </div>
-                                <div className="col-md">
-                                    <a className="footer-link" href="/home#report-a-bug"><h5>Report a bug</h5></a>
+                            </div>
+                            <div className={styles.main}>
+                                {props.children}
+                                <div className={styles.footer}>
+                                    <div className="row justify-content-around">
+                                        <div className="col-md offset-md-1">
+                                            <a className="footer-link" href="/home#behind-the-music"><h5>About</h5></a>
+                                            <ul className="list-unstyled text-small">
+                                                <a className="footer-link" href="/home#collaborators"><li>Collaborators</li></a>
+                                                <a className="footer-link" href="/home#sponsors"><li>Sponsors</li></a>
+                                                <a className="footer-link" href="/home#contact"><li>Contact us</li></a>
+
+                                            </ul>
+                                        </div>
+                                        <div className="col-md">
+                                            <a className="footer-link" href="/getting_started#how-to-use"><h5>How to use</h5></a>
+                                        </div>
+                                        <div className="col-md">
+                                            <a className="footer-link" href="/home#report-a-bug"><h5>Report a bug</h5></a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </DocumentMeta>
+                </DocumentMeta>
+            )}
+        />
     )
 }
 
