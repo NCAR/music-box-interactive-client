ddd922af9e5cae624fcc1d3262183879e5ed479a
Shane Ng <shanengys@gmail.com>
2023-07-05 02:19:17 -0700

    refactored instruction

--- generator/src/components/Mechanism/GasSpeciesTab.js
+++ generator/src/components/Mechanism/GasSpeciesTab.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Link } from "gatsby";
 import GasSpeciesDetail from "./GasSpeciesDetail.js";
 import GasSpeciesList from "./GasSpeciesList.js";
 
@@ -10,7 +9,7 @@ function GasSpeciesTab(props) {
 
   return (
     <>
-      <p className="lead-muted p-2">Select a chemical species from the list to view/edit its properties, or add a new chemical species to the mechansim. The chemical species you add here will be available to participate in <Link to="mechanism/reactions">reactions</Link> and can be include in the <Link to="conditions">model conditions</Link>.</p>
+      <SpeciesInstruction />
       <div id="species-content" className="container-fluid p-2 d-flex flex-column overflow-hidden">
         <div className="row flex-grow-1 overflow-hidden">
           <div className="col-md-4 col-lg-4 mh-100 overflow-auto">
