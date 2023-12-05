cba68f28c27bdfcf157d8674fdefda0cb4fdfa3b
Kyle Shores <kyle.shores44@gmail.com>
2023-12-05 10:12:02 -0600

    partmc feature flags

--- src/pages/mechanism.jsx
+++ src/pages/mechanism.jsx
@@ -3,11 +3,23 @@ import { connect } from 'react-redux';
 import TabbedLayout from "../components/TabbedLayout";
 import { MechanismTab } from "../components/Mechanism";
 
-const tabs = [
-  { label: "Gas Species", component: () => <MechanismTab type="gas" /> },
-  { label: "Aerosol Species", component: () => <MechanismTab type="aerosol" /> },
-  { label: "Reactions", component: () => <MechanismTab type="reactions" /> },
+const featureFlags = JSON.parse(import.meta.env.VITE_FEATURE_FLAGS || '{}');
+
+const gas = { label: "Gas Species", component: () => <MechanismTab type="gas" /> };
+const aerosols = { label: "Aerosol Species", component: () => <MechanismTab type="aerosol" /> };
+const reactions = { label: "Reactions", component: () => <MechanismTab type="reactions" /> };
+let tabs = [
+  gas,
+  reactions
 ];
+
+if (featureFlags.PART_MC) {
+  tabs = [
+    gas,
+    aerosols,
+    reactions
+  ];
+}
 
 function Mechanism() {
   return (
