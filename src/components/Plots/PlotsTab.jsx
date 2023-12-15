515687bd4ef06aae95f8585f2326446852c694f5
Kyle Shores <kyle.shores44@gmail.com>
2023-12-15 11:13:40 -0600

    removing stuff

--- src/components/Plots/PlotsTab.jsx
+++ src/components/Plots/PlotsTab.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import { connect } from "react-redux";
 import PlotSelector from "./PlotSelector";
 import Plot from "./Plot";
-import { getPlotsByType, getTemperatureData } from "../../redux/selectors";
-import ScatterPlot from "./ScatterPlot"
+import { getPlotsByType } from "../../redux/selectors";
 
 const PlotsTab = (props) => {
   return (
     <div className="container-fluid p-2 d-flex flex-column vh-100 overflow-hidden">
-      {/* <LinePlot data={props.temperature.slice(0, 20)} label="Temperature" units="K"/>
-      <ScatterPlot data={props.temperature.slice(0, 20)} label="Temperature" units="K"/> */}
       <div className="row flex-grow-1 overflow-hidden">
         <div className="col-md-4 col-lg-3 mh-100 overflow-auto">
           <div className="row flex-shrink-0">
@@ -34,7 +31,6 @@ const PlotsTab = (props) => {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    temperature: getTemperatureData(state),
     plots: getPlotsByType(state, ownProps.plotType),
   };
 };
