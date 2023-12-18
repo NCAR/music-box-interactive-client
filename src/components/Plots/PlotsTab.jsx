175cdcf345d2ff49df726f86de16b1d653b76858
mattldawson <mattldawson@users.noreply.github.com>
2023-12-18 17:12:01 +0000

    prettier action fixes

--- src/components/Plots/PlotsTab.jsx
+++ src/components/Plots/PlotsTab.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import PlotSelector from "./PlotSelector";
 import { getPlotsByType, getPlotDataByType } from "../../redux/selectors";
@@ -31,7 +31,7 @@ const PlotsTab = (props) => {
                 activeIndex={activeIndex}
                 setActiveIndex={setActiveIndex}
               />
-            )
+            );
           })}
         </div>
       </div>
