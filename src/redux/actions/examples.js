3dfc064eee56b68cc82f00c292c5ed4efa76bd86
K20shores <K20shores@users.noreply.github.com>
2023-12-22 16:05:20 +0000

    prettier action fixes

--- src/redux/actions/examples.js
+++ src/redux/actions/examples.js
@@ -29,9 +29,12 @@ export const getExample = (example) => async (dispatch) => {
 export const loadConfiguration = (file) => async (dispatch) => {
   try {
     const data = await fetchConfiguration(file);
-    const mechanism = extract_mechanism_from_example(data)
-    const conditions = extract_conditions_from_example(data, mechanism)
-    dispatch({ type: utils.action_types.EXAMPLE_FETCHED, payload: {mechanism: mechanism, conditions: conditions} });
+    const mechanism = extract_mechanism_from_example(data);
+    const conditions = extract_conditions_from_example(data, mechanism);
+    dispatch({
+      type: utils.action_types.EXAMPLE_FETCHED,
+      payload: { mechanism: mechanism, conditions: conditions },
+    });
   } catch (error) {
     const msg = `Error loading configuration: ${error.message}. If you believe your configuraiton is valid, please submit a bug at https://github.com/NCAR/music-box-interactive-client/issues/new?assignees=&labels=&projects=&template=bug_report.md&title=`;
     console.error(msg);
