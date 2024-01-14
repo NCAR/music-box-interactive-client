d24ab993340c6d83c68e79c882a7e28e0383e61a
Ketan Sahu <120404520+ketansahugit@users.noreply.github.com>
2024-01-14 15:30:25 +0000

    add react-bootstrap select component

--- src/components/Conditions/EvolvingConditionsDetail.jsx
+++ src/components/Conditions/EvolvingConditionsDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Table, Container, Row, Col, Card } from "react-bootstrap";
+import { Table, Container, Row, Col, Card, Form } from "react-bootstrap";
 import { getEvolvingConditions } from "../../redux/selectors";
 import {
   resortEvolvingConditions,
@@ -69,20 +69,19 @@ const EvolvingConditionsDetail = (props) => {
         </Row>
         <Row>
           <Col>
-            <label className="my-4">
-              Items Per Page:
-              <select
-                className="mx-2"
-                value={itemsPerPage}
-                onChange={(e) =>
-                  handleItemsPerPageChange(Number(e.target.value))
-                }
-              >
-                <option value={50}>50</option>
-                <option value={100}>100</option>
-                <option value={200}>200</option>
-              </select>
-            </label>
+           <Form.Group controlId="itemsPerPageSelect" className="my-4">
+            <Form.Label className="fw-bold">Items Per Page :</Form.Label>
+            <Form.Select
+              className="mx-2"
+              value={itemsPerPage}
+              onChange={(e) => handleItemsPerPageChange(Number(e.target.value))}
+              aria-label="Select Items Per Page"
+            >
+              <option value={50}>50</option>
+              <option value={100}>100</option>
+              <option value={200}>200</option>
+            </Form.Select>
+           </Form.Group>
           </Col>
         </Row>
       </Container>
