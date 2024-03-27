0a68be2a34c9978f15245c2003746ee856993568
Kyle Shores <kyle.shores44@gmail.com>
2024-03-27 09:38:49 -0500

    allowing conditions to be set

--- src/components/Conditions/ConditionsList.jsx
+++ src/components/Conditions/ConditionsList.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { connect } from "react-redux";
 import AddCondition from "./AddCondition";
 import { getConditions, getUserDefinedRatesIds } from "../../redux/selectors";
+import { getMechanism } from "../../redux/selectors";
 
 const ConditionsList = (props) => {
   return (
     <div className="card mb-4 p-0 shadow-sm">
       <div className="card-header d-flex justify-content-between">
         <h4 className="my-0">{props.schema.label}</h4>
-        {props.schema.allowAddRemove && props.possibleReactions.length > 0 ? (
+        {props.schema.allowAddRemove && props.remaining_unused > 0 ? (
           <AddCondition schema={props.schema} />
         ) : null}
       </div>
@@ -38,11 +39,22 @@ const ConditionsList = (props) => {
 const mapStateToProps = (state, ownProps) => {
   const { schema } = ownProps;
   const conditions = getConditions(state, schema);
-  const reactionIds = conditions.map((condition) => condition.reactionId);
-  const possibleReactions = getUserDefinedRatesIds(state).filter(
-    (reaction) => !reactionIds.includes(reaction.id),
-  );
-  return { conditions, possibleReactions };
+
+  // keep track of how many conditions can still be added
+  let remaining_unused = 0;
+
+  if (schema.classKey === "initial_species_concentrations") {
+    const mechanism = getMechanism(state);
+    // -1 below because M is always a species in the gas phase
+    // but you cannot set its concentration
+    remaining_unused = mechanism.gasSpecies.length - 1 - conditions.length;
+  }
+  else if (schema.classKey === "initial_reactions") {
+    const reactionIds = conditions.map((condition) => condition.reactionId);
+    const possibleReactions = getUserDefinedRatesIds(state)
+    remaining_unused = possibleReactions.length - reactionIds.length;
+  }
+  return { conditions, remaining_unused };
 };
 
 export default connect(mapStateToProps)(ConditionsList);
