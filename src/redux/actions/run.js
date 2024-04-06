5e21b1e49b0f0f85c4347f55f698f699454e4867
Evan De la Garza <edelagarza2002@gmail.com>
2024-04-06 12:55:53 -0500

    added back api run in case necessary

--- src/redux/actions/run.js
+++ src/redux/actions/run.js
@@ -1,4 +1,5 @@
 import utils from "../utils";
+import { run } from "../../controllers/api";
 import {
   translate_to_camp_config,
   translate_to_musicbox_conditions,
@@ -9,10 +10,14 @@ export const doRun = (mechanism, conditions) => async (dispatch) => {
     const camp_mechanism = translate_to_camp_config(mechanism);
     const musicbox_conditions = translate_to_musicbox_conditions(conditions, mechanism);
 
+    // run python script
     const script = './src/scripts/print_config.py';
     const args = [JSON.stringify({ mechanism: camp_mechanism, conditions: musicbox_conditions })];
 
     await window.electron.doRun(script, args);
+
+    // api run
+    await run({ mechanism: camp_mechanism, conditions: musicbox_conditions });
 
     dispatch({ type: utils.action_types.START_POLLING, payload: {} });
   } catch (error) {
