ca40295cdafa4931afbe30e4d5bc8e8c91d02b4c
walkerdrury1 <walkerdrury11@gmail.com>
2024-04-10 13:01:36 -0500

    put back web app functionality

--- src/redux/actions/run.js
+++ src/redux/actions/run.js
@@ -4,25 +4,27 @@ import {
   translate_to_camp_config,
   translate_to_musicbox_conditions,
 } from "../../controllers/transformers";
+import isElectron from "is-electron";
 
 export const doRun = (mechanism, conditions) => async (dispatch) => {
   try {
     const camp_mechanism = translate_to_camp_config(mechanism);
     const musicbox_conditions = translate_to_musicbox_conditions(conditions, mechanism);
 
-    // run python script
-    const script = 'print_config.py';
-    const args = [JSON.stringify({ mechanism: camp_mechanism, conditions: musicbox_conditions })];
-    
-    //output from python solver
-    const boxModelOutput = await window.electron.doRun(script, args);
+    if(isElectron()) {
+      const script = 'print_config.py';
+      const args = [JSON.stringify({ mechanism: camp_mechanism, conditions: musicbox_conditions })];
+      
+      //output from python solver
+      const boxModelOutput = await window.electron.doRun(script, args);
+      dispatch({type: utils.action_types.RESULTS_LOADED, payload: {boxModelOutput}})
+
+    } else {
+      await run({ mechanism: camp_mechanism, conditions: musicbox_conditions });
+      dispatch({ type: utils.action_types.START_POLLING, payload: {} });
+    }
    
 
-
-    // api run
-    await run({ mechanism: camp_mechanism, conditions: musicbox_conditions });
-
-    dispatch({ type: utils.action_types.START_POLLING, payload: {boxModelOutput} });
   } catch (error) {
     console.error(`Error starting run: ${error.message}`);
   }
