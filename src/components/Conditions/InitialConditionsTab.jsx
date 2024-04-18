9d8cfcdb4b491ed1871f00115c7e601518fe4761
Kyle Shores <kyle.shores44@gmail.com>
2024-04-18 11:00:48 -0500

    sorting conditions by name

--- src/components/Conditions/InitialConditionsTab.jsx
+++ src/components/Conditions/InitialConditionsTab.jsx
@@ -4,6 +4,7 @@ import { initialConditionsSchema } from "../../redux/schemas";
 import ConditionsList from "./ConditionsList";
 
 function InitialConditionsTab(props) {
+  const [ speciesConditionSchema, environmentalConditionSchema, reactionConditionSchema ] = initialConditionsSchema
   return (
     <>
       <p className="lead-muted p-2">
@@ -11,16 +12,18 @@ function InitialConditionsTab(props) {
         and reaction rates/rate constants that have a MUSICA name can be set
         here. The conditions you set here will remain at the value you specify
         until updated by the solver (as is the case for chemical species
-        concentrations) or overwritten by evolving conditions you specify.
+          concentrations) or overwritten by evolving conditions you specify.
       </p>
       <div className="configbox container-fluid">
-        {initialConditionsSchema.map((schema, index) => {
-          return (
-            <div className="row m-2" key={`conditions-list-${index}`}>
-              <ConditionsList schema={schema} />
-            </div>
-          );
-        })}
+        <div className="row m-2">
+          <ConditionsList schema={speciesConditionSchema} />
+        </div>
+        <div className="row m-2">
+          <ConditionsList schema={environmentalConditionSchema} />
+        </div>
+        <div className="row m-2">
+          <ConditionsList schema={reactionConditionSchema} />
+        </div>
       </div>
     </>
   );
