c25e61a566bd6a9ea0cbe4dda6b972a639bd8f34
Brendan Fattig <brendan@fattig.net>
2024-04-23 14:02:03 -0500

    bundle python merged

--- app/main/main.js
+++ app/main/main.js
@@ -55,7 +55,7 @@ ipcMain.handle("run-python", async (event, script, args) => {
   const configFileDir = path.join(
     appDataPath,
     "previous_results",
-    `mechanism-${Date.now()}`,
+    `mechanism-${Date.now()}`
   );
 
   await fs.promises.mkdir(configFileDir, { recursive: true });
@@ -75,16 +75,34 @@ ipcMain.handle("run-python", async (event, script, args) => {
       app.getAppPath(),
       "src",
       "scripts",
-      "print_config.py",
+      "print_config.py"
     );
   }
 
-  const command = `python ${scriptPath} ${configFilePath} ${configFileDir}`;
+  function findPython() {
+    const possibilities = [
+      // In packaged app
+      path.join(process.resourcesPath, "python", "bin", "python"),
+      // In development
+      path.join(__dirname, "python", "bin", "python"),
+    ];
+    for (const path of possibilities) {
+      if (fs.existsSync(path)) {
+        return path;
+      }
+    }
+    console.log("Could not find python3, checked", possibilities);
+    app.quit();
+  }
+
+  const pythonPath = findPython();
+
+  const command = `${pythonPath} ${scriptPath} ${tempFilePath}`;
   console.log(`Full command: ${command}`);
 
   try {
     return new Promise((resolve, reject) => {
-      const python = spawn("python", [
+      const python = spawn(pythonPath, [
         scriptPath,
         configFilePath,
         configFileDir,
@@ -139,7 +157,7 @@ ipcMain.handle("load-example", async (event, example) => {
       app.getAppPath(),
       "src",
       "examples",
-      exampleFile,
+      exampleFile
     );
   }
 
@@ -207,7 +225,7 @@ ipcMain.handle("load-previous-config", async (event, dir) => {
       try {
         const configFileContent = fs.readFileSync(
           path.join(filePath, configFile),
-          "utf-8",
+          "utf-8"
         );
         let config = JSON.parse(configFileContent);
 
